085c2e237cbe6d875f17361d540f10b6026f4f10
okezieuc <okezieuc@users.noreply.github.com>
2023-12-02 10:37:39 -0600

    create /notes route to create notes
    this route:
    - creates an entry in the D1 database that maps ids to the relevant text
    - generates a vector embedding for the text
    - creates a vectorize entry that relates the text's vector embedding and
    its id in the d1 table

--- src/index.js
+++ src/index.js
@@ -12,4 +12,38 @@ app.get('/', async (c) => {
 	return c.json(answer);
 });
 
+app.post('/notes', async (c) => {
+	const ai = new Ai(c.env.AI);
+
+	const { text } = await c.req.json();
+	if (!text) {
+		return c.text('Missing text', 400);
+	}
+
+	const { results } = await c.env.DB.prepare('INSERT INTO notes (text) VALUES (?) RETURNING *').bind(text).run();
+
+	const record = results.length ? results[0] : null;
+
+	if (!record) {
+		return c.text('Failed to create note', 500);
+	}
+
+	const { data } = await ai.run('@cf/baai/bge-base-en-v1.5', { text: [text] });
+	const values = data[0];
+
+	if (!values) {
+		return c.text('Failed to generate vector embedding', 500);
+	}
+
+	const { id } = record;
+	const inserted = await c.env.VECTOR_INDEX.upsert([
+		{
+			id: id.toString(),
+			values,
+		},
+	]);
+
+	return c.json({ id, text, inserted });
+});
+
 export default app;
